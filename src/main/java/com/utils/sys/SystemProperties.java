07c833646799dacad8295f7aa0f7f19005b20c2a
shentar <tyanqun@mail.com>
2017-07-01 22:26:38 +0800

    add maven scripts.

---
+++ src/main/java/com/utils/sys/SystemProperties.java
@@ -0,0 +1,45 @@
+package com.utils.sys;
+
+import java.util.HashMap;
+
+public final class SystemProperties
+{
+    private ThreadLocal<HashMap<String, Object>> contentLocal;
+
+    private static SystemProperties instance = new SystemProperties();
+
+    static
+    {
+        instance.init();
+    }
+
+    public static void add(String key, Object value)
+    {
+        instance.contentLocal.get().put(key, value);
+    }
+
+    public static Object get(String key)
+    {
+        return instance.contentLocal.get().get(key);
+    }
+
+    private void init()
+    {
+        contentLocal = new ThreadLocal<HashMap<String, Object>>()
+        {
+            public HashMap<String, Object> initialValue()
+            {
+                return new HashMap<String, Object>();
+            }
+        };
+    }
+
+    public static void clear()
+    {
+        instance.contentLocal.get().clear();
+    }
+
+    private SystemProperties()
+    {
+    }
+}
